44474195af0f1c778828f4c975d5d7797fc58051
tokoblotongan <blotonganjayalengkap@gmail.com>
2026-05-27 08:53:19 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 800 },
+            debug: false
+        }
+    },
+    scene: [SceneGame]
+};
 
+const game = new Phaser.Game(config);
